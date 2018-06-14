b617e4b778c60256c3d9ec075f43c7ebe7368684
Igor Lins e Silva <igor.linsesilva@gmail.com>
2018-06-13 22:26:52 -0300

    mac icon fix

--- src/app/network.service.ts
+++ src/app/network.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NetworkService {
 
-  constructor() { }
+  publicEndpoints: any[];
+
+  constructor() {
+  }
+
 }
