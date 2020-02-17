8585ee5769e780ad5f24273305bcd987beefd558
Igor Lins e Silva <igor.linsesilva@gmail.com>
2020-02-16 21:56:59 -0300

    cleanup

--- build-liberland.js
+++ build-liberland.js
@@ -9,8 +9,8 @@ conf.description = 'Liberland Blockchain Wallet';
 conf.build.appId = 'liberland-wallet';
 conf.build.win.icon = "src/assets/liberland_256_LdC_icon.ico";
 conf.build.dmg.icon = "src/assets/icons/liberland256x256.icns";
-conf.build.mac.icon = "liberland518x518.png";
-conf.build.linux.icon = "liberland256x256.png";
+conf.build.mac.icon = "other_assets/liberland518x518.png";
+conf.build.linux.icon = "other_assets/liberland256x256.png";
 
 fs.writeFileSync('./package.json', JSON.stringify(conf, null, "\t"));
 
--- build-simpleos.js
+++ build-simpleos.js
@@ -9,9 +9,9 @@ conf.productName = 'simpleos';
 conf.description = 'EOSIO Blockchain Interface & Wallet';
 conf.build.appId = 'simpleos';
 conf.build.win.icon = "src/favicon.ico";
-conf.build.mac.icon = "icon.png";
+conf.build.mac.icon = "other_assets/icon.png";
 conf.build.dmg.icon = "src/assets/icons/256x256.icns";
-conf.build.linux.icon = "256x256.png";
+conf.build.linux.icon = "other_assets/256x256.png";
 
 fs.writeFileSync('./package.json', JSON.stringify(conf, null, "\t"));
 
