9cb5d84ad68fa3a1de46b43d9e89ce2d36ec6d0e
Igor Lins e Silva <igor.linsesilva@gmail.com>
2020-02-16 23:09:05 -0300

    fix transit callback when cancelled

--- electron_modules/transit-api.js
+++ electron_modules/transit-api.js
@@ -22,67 +22,89 @@ class TransitApiService {
     this.setupRoutes();
   }
 
+  sendMessage(channel, data) {
+    this.main.win.webContents.send(channel, data);
+  }
+
+  setJsonHeader(res) {
+    res.setHeader('Content-Type', 'application/json');
+  }
+
+  reply(res, data) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(data));
+  }
+
   setupRoutes() {
     this.expressApp.use(cors());
+
+    // ping
     this.expressApp.get('/ping', (req, res) => {
       res.end('OK');
     });
+
+    // accounts
     this.expressApp.get('/accounts', (req, res) => {
-      this.main.win.webContents.send('request', 'accounts');
+      this.sendMessage('request', 'accounts');
       ipcMain.once('accountsResponse', (event, data) => {
-        console.log(data);
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(data));
+        this.reply(res, data);
       });
     });
+
+    // get public keys
     this.expressApp.get('/getPublicKeys', (req, res) => {
-      this.main.win.webContents.send('request', {
+      this.sendMessage('request', {
         message: 'publicKeys',
       });
       ipcMain.once('publicKeyResponse', (event, data) => {
-        console.log(data);
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(data));
+        this.reply(res, data);
       });
     });
+
+    // connect
     this.expressApp.get('/connect', (req, res) => {
+
       console.log('CONNECT REQUEST');
       this.main.getFocus();
-      this.main.win.webContents.send('request', {
+
+      this.sendMessage('request', {
         message: 'connect',
         content: {
           appName: req.query['appName'],
           chainId: req.query['chainId'],
         },
       });
-      if (req.query['appName'].length < 32 && req.query['chainId'].length ===
-          64) {
+
+      if (req.query.appName.length < 32 && req.query.chainId.length === 64) {
         ipcMain.once('connectResponse', (event, data) => {
-          console.log(data);
-          res.setHeader('Content-Type', 'application/json');
-          res.end(JSON.stringify(data));
+          this.reply(res, data);
         });
       }
     });
+
+    // login
     this.expressApp.get('/login', (req, res) => {
       console.log('CONNECT REQUEST, account:' + req.query.account);
-      this.main.win.webContents.send('request', {
+
+      this.sendMessage('request', {
         message: 'login',
         content: {
           account: req.query.account,
         },
       });
+
       if (!req.query.account) {
         this.main.getFocus();
       }
+
       if (req.query.account) {
         if (req.query.account.length > 13) {
           res.end('ERROR');
           return false;
         }
       }
+
       ipcMain.once('loginResponse', (event, data) => {
-        console.log(data);
         if (data.status) {
           if (data.status !== 'CANCELLED') {
             if (!req.query.account) {
@@ -92,55 +114,63 @@ class TransitApiService {
         } else {
           this.main.unfocus();
         }
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(data));
+        this.reply(res, data);
       });
     });
+
+    // logout
     this.expressApp.get('/logout', (req, res) => {
       console.log('LOGOUT REQUEST');
-      this.main.win.webContents.send('request', {
+
+      this.sendMessage('request', {
         message: 'logout',
         content: {
           account: req.query.account,
         },
       });
+
       if (req.query.account) {
         if (req.query.account.length > 13) {
           res.end('ERROR');
           return false;
         }
       }
+
       ipcMain.once('logoutResponse', (event, data) => {
-        console.log(data);
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(data));
+        this.reply(res,data);
       });
     });
+
+    // disconnect
     this.expressApp.get('/disconnect', (req, res) => {
       console.log('DISCONNECT REQUEST');
-      this.main.win.webContents.send('request', {
+
+      this.sendMessage('request', {
         message: 'disconnect',
       });
+
       ipcMain.once('disconnectResponse', (event, data) => {
-        console.log(data);
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(data));
+        this.reply(res,data);
       });
     });
+
+    // sign
     this.expressApp.post('/sign', bodyParser.json(), (req, res) => {
-      this.main.win.webContents.send('request', {
+
+      this.sendMessage('request', {
         message: 'sign',
         content: req.body,
       });
+
       this.main.getFocus();
+
       ipcMain.once('signResponse', (event, data) => {
-        console.log(data);
-        res.setHeader('Content-Type', 'application/json');
         if (data.status !== 'CANCELLED') {
           this.main.unfocus();
         }
-        res.end(JSON.stringify(data));
+        this.reply(res,data);
       });
+
     });
   }
 
