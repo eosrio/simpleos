ef1b58f83e99cd29f22f0a8dc696f32e2d146165
Igor Lins e Silva <igor.linsesilva@gmail.com>
2020-02-19 19:17:37 -0300

    fix postinstall script

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,16 +1,16 @@
-// Allow angular using electron module (native node modules)
 const fs = require('fs');
+
+// patch webpack browser config for electron
 const f_angular = 'node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/browser.js';
+const data = fs.readFileSync(f_angular, 'utf8');
+if (!data) {
+    return console.log('failed to read:' + f_angular);
+}
+let result = data.replace(/target: "electron-renderer",/g, '');
+result = result.replace(/target: "web",/g, '');
+result = result.replace(/return {/g, 'return {target: "electron-renderer",');
+result = result.replace(/node: false/g, 'node: {crypto: true, stream: true}');
+fs.writeFileSync(f_angular, result, 'utf8');
 
-fs.readFile(f_angular, 'utf8', function (err, data) {
-  if (err) {
-    return console.log(err);
-  }
-  let result = data.replace(/target: "electron-renderer",/g, '');
-  result = result.replace(/target: "web",/g, '');
-  result = result.replace(/return {/g, 'return {target: "electron-renderer",');
-
-  fs.writeFile(f_angular, result, 'utf8', function (err) {
-    if (err) return console.log(err);
-  });
-});
+// Copy electron typings
+fs.copyFileSync('src/types/electron.bak', 'node_modules/electron/electron.d.ts');
